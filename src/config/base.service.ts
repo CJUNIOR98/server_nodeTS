0f55e924acb21f8ad49ac9cc8ca6a007240c6856
Carlos Mejia <carlosjuniorm360@gmail.com>
2022-12-04 17:09:58 -0500

    Fixed typing errors in the code

--- src/config/base.service.ts
+++ src/config/base.service.ts
@@ -1,15 +1,15 @@
-import { EntityTarget, Repository } from "typeorm";
 import { BaseEntity } from "./base.entity";
 import { ConfigServer } from "./config";
+import { Repository, EntityTarget } from 'typeorm';
 
 export class BaseService<T extends BaseEntity> extends ConfigServer {
-  public execRepository: Promise<Repository<T>>;
+  public execRepository: Promise<Repository<T>>
   constructor(private getEntity: EntityTarget<T>) {
     super();
     this.execRepository = this.initRepository(getEntity);
   }
 
-  async initRepository<T>(e: EntityTarget<T>): Promise<Repository<T>> {
+  async initRepository(e: EntityTarget<T>): Promise<Repository<T>> {
     const getConn = await this.initConnect;
     return getConn.getRepository(e);
   }
