6dc4cd5db32f7e2b00e0da2423a916b29c98920a
Carlos Mejia <carlosjuniorm360@gmail.com>
2022-12-04 21:06:59 -0500

    Added function to search users by role

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,7 +1,7 @@
 import { DeleteResult, UpdateResult } from "typeorm";
 import * as bcrypt from 'bcrypt';
 import { BaseService } from "../../config/base.service";
-import { UserDTO } from "../dto/user.dto";
+import { RoleType, UserDTO } from "../dto/user.dto";
 import { UserEntity } from "../entities/user.entity";
 
 export class UserService extends BaseService<UserEntity> {
@@ -15,6 +15,19 @@ export class UserService extends BaseService<UserEntity> {
 
   async findUserById(id: string): Promise<UserEntity | null> {
     return (await this.execRepository).findOneBy({ id });
+  }
+
+  async findUserWhitRole(
+    id: string,
+    role: RoleType
+  ): Promise<UserEntity | null> {
+    const user = (await this.execRepository)
+      .createQueryBuilder("user")
+      .where({ id })
+      .andWhere({ role })
+      .getOne();
+
+    return user;
   }
 
   async findUserWithRelation(id: string): Promise<UserEntity | null> {
